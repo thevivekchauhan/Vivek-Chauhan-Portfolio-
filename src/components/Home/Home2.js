ccf4da58e140357a636c7a20ffe974944aaadc50
thevivekchauhan <165350621+thevivekchauhan@users.noreply.github.com>
2024-03-29 14:15:54 +0530

    New Portfolio

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,116 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/vc2.png";
+import Tilt from "react-parallax-tilt";
+import {
+  // AiFillGithub,
+  // AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <br></br><br></br><br></br><br></br><br></br>
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              I fell in love with programming and I have at least learnt
+              something, I think… 🤷‍♂️
+              <br />
+              <br />I am good in
+              <i>
+                <b className="purple"> HTML, CSS, Javascript, React JS, Ant Design. </b>
+              </i>
+              <br />
+              <br />
+              I finished my graduation in  &nbsp;
+              <i>
+                <b className="purple">BCA  </b> 
+                 it means{" "}
+                <b className="purple">
+                Bachelor of Computer Application.
+                </b>
+              </i>
+              <br />
+              <br />
+              Currently I am working in a company as a .
+               <b className="purple">Frontend Developer</b> and
+              <i>
+                <i>
+                  {" "}
+                  Which name is 
+                </i>
+              </i>
+              &nbsp; 
+              <i>
+                <b className="purple"> Bitbeast private limited company.</b>
+              </i>
+            </p>
+          </Col>
+          <Col md={4 } className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              {/* <li className="social-icons">
+                <a
+                  href="https://github.com/soumyajit4419"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://twitter.com/Soumyajit4419"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiOutlineTwitter />
+                </a>
+              </li> */}
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/vivekchauhan005?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/the_vivekchauhan"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
