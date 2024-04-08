defd7933062635bd32104336008c303b60fdf133
thevivekchauhan <165350621+thevivekchauhan@users.noreply.github.com>
2024-04-08 09:57:45 +0530

    change home-bg1.jpg and add download button in resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Button} from "react-bootstrap";
 // import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/vc_resume.pdf";
-// import { AiOutlineDownload } from "react-icons/ai";
+import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
@@ -19,7 +19,7 @@ function ResumeNew() {
     <div>
       <Container fluid className="resume-section">
         <Particle />
-        {/* <Row style={{ justifyContent: "center", position: "relative" }}>
+        <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
             href={pdf}
@@ -29,7 +29,7 @@ function ResumeNew() {
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
-        </Row> */}
+        </Row>
 
         <Row className="resume">
           <Document file={pdf} className="d-flex justify-content-center">
@@ -47,7 +47,6 @@ function ResumeNew() {
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button> */}
-           <br></br><br></br><br></br><br></br><br></br><br></br> 
         </Row>
       </Container>
     </div>
